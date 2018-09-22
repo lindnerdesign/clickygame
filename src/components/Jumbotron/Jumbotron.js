6e1311157e4c1e7eaebe3834a9addb9eecce0c33
lindnerdesign <lindnerdesign@gmail.com>
2018-09-22 12:12:34 -0500

    deploy103

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -1,12 +1,13 @@
 import React from "react"
 import { Jumbotron } from "react-bootstrap";
 import './JumbotronStyle.css';
+import title from './title.png'
 
 class Header extends React.Component {
    render(){
    return(
     <Jumbotron className="Jumbotron">
-      <img src="/images/title.png" className="header-one"></img>
+      <img src= {title} className="header-one"></img>
     </Jumbotron>
    );
   }
